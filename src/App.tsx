3c180d9f3ceb16a6596a1f641fde32948337c550
Igor Trifonov <Ha1lowe3n@yandex.ru>
2022-01-12 21:38:00 +0800

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [opacityText, setOpacityText] = useState<boolean>(true);
+    const [displayText, setDisplayText] = useState<boolean>(true);
+
+    const offOpacityText = () => setOpacityText(false);
+    const deleteText = (e: globalThis.KeyboardEvent) => {
+        if (e.key === " ") {
+            setDisplayText(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("click", offOpacityText);
+        window.addEventListener("keydown", deleteText);
+
+        return () => {
+            window.removeEventListener("click", offOpacityText);
+            window.removeEventListener("keydown", deleteText);
+        };
+    }, []);
+
+    return (
+        <main>
+            <div
+                id="text"
+                className={`${!opacityText ? "opacity-0" : ""}${
+                    !displayText ? " display-none" : ""
+                }`}
+            >
+                <h2>Trifonov Igor</h2>
+                <span>
+                    Начинающий web-программист / Junior Frontend Developer
+                </span>
+            </div>
+        </main>
+    );
 }
-
-export default App;
